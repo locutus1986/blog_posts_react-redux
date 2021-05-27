bbd792700668eef9a4ebd72112413a3126c631c5
Zachary Ruffner <locutus1986@gmail.com>
2021-05-27 13:06:22 -0400

    last

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,24 +2,24 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder'
 import _ from 'lodash'
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-        await dispatch(fetchPosts())
-        
-        _.chain(getState().posts)
-                .map('userId')
-                .uniq()
-                .forEach(id => dispatch(fetchUser(id)))
-                .value()
+    await dispatch(fetchPosts())
+    
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
 }
 
 export const fetchPosts = () => async dispatch =>{
-        const res = await jsonPlaceHolder.get('/posts')
+    const res = await jsonPlaceHolder.get('/posts')
 
-        dispatch({ type: 'FETCH_POSTS',payload:res.data })
+    dispatch({ type: 'FETCH_POSTS',payload:res.data })
 }
 
 export const fetchUser = id => async dispatch => {
-        const res = await jsonPlaceHolder.get(`/users/${id}`)
-        dispatch({ type: 'FETCH_USER', payload:res.data })
+    const res = await jsonPlaceHolder.get(`/users/${id}`)
+    dispatch({ type: 'FETCH_USER', payload:res.data })
 }
 
 
